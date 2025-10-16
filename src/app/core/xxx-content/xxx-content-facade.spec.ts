13db04156ae898db02ee56b4089b9bedffc01482
angularexample <info@angularexample.com>
2025-10-16 12:10:30 -0400

    add xxx-content-facade.spec.ts, 100% coverage

--- src/app/core/xxx-content/xxx-content-facade.spec.ts
+++ src/app/core/xxx-content/xxx-content-facade.spec.ts
@@ -1,62 +1,92 @@
+import { mockContentHome } from './xxx-content.mocks';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { selectContentByKey, selectIsContentEmpty, selectIsContentError } from './xxx-content.selectors';
 import { TestBed } from '@angular/core/testing';
 import { XxxContentFacade } from './xxx-content-facade';
-import { XxxContentStore } from './xxx-content-store';
+import { xxxContentInitialState } from './xxx-content-types';
 
 describe('XxxContentFacade', () => {
-  const mockXxxContentStore = {
-    contentByKey: jest.fn(),
-    isContentEmpty: jest.fn(),
-    isContentError: jest.fn(),
-    showContent: jest.fn(),
-  };
-  const contentKey: string = 'content-key';
+  const contentKey: string = 'home';
 
   TestBed.configureTestingModule({
     providers: [
-      XxxContentFacade,
-      {provide: XxxContentStore, useValue: mockXxxContentStore},
-    ],
+      provideMockStore({
+        initialState: xxxContentInitialState,
+        selectors: [
+          {
+            selector: selectContentByKey(contentKey),
+            value: mockContentHome
+          },
+          {
+            selector: selectIsContentEmpty(contentKey),
+            value: false
+          },
+          {
+            selector: selectIsContentError(contentKey),
+            value: true
+          }
+        ]
+      }),
+      XxxContentFacade
+    ]
   });
 
   const service: XxxContentFacade = TestBed.inject(XxxContentFacade);
+  const store: MockStore = TestBed.inject(MockStore);
+
+  afterEach(() => {
+    //store.resetSelectors();
+  });
 
   describe('constructor phase', () => {
     it('should be created', () => {
       expect(service).toBeDefined();
     });
 
-    it('should have contentByKey', () => {
-      expect(service.contentByKey).toBeDefined();
+    it('should have contentByKey$', () => {
+      expect(service.contentByKey$).toBeDefined();
     });
 
-    it('should have isContentEmpty', () => {
-      expect(service.isContentEmpty).toBeDefined();
+    it('should have isContentEmpty$', () => {
+      expect(service.isContentEmpty$).toBeDefined();
     });
 
-    it('should have isContentError', () => {
-      expect(service.isContentError).toBeDefined();
+    it('should have isContentError$', () => {
+      expect(service.isContentError$).toBeDefined();
     });
-  })
+  });
 
-  describe('methods', () => {
-    it('should run contentStore.contentByKey', () => {
-      service.contentByKey(contentKey);
-      expect(mockXxxContentStore.contentByKey).toHaveBeenCalledWith(contentKey);
+  describe('contentByKey$', () => {
+    it('should run store.select(selectContentByKey)', () => {
+      service.contentByKey$(contentKey).subscribe((result) => {
+        expect(result).toEqual(mockContentHome);
+      });
     });
+  });
 
-    it('should run contentStore.isContentEmpty', () => {
-      service.isContentEmpty(contentKey);
-      expect(mockXxxContentStore.isContentEmpty).toHaveBeenCalledWith(contentKey);
+  describe('isContentEmpty$', () => {
+    it('should run store.select(selectIsContentEmpty)', () => {
+      service.isContentEmpty$(contentKey).subscribe((result) => {
+        expect(result).toBe(false);
+      });
     });
+  });
 
-    it('should run contentStore.isContentError', () => {
-      service.isContentError(contentKey);
-      expect(mockXxxContentStore.isContentError).toHaveBeenCalledWith(contentKey);
+  describe('isContentError$', () => {
+    it('should run store.select(selectIsContentError)', () => {
+      service.contentByKey$(contentKey).subscribe((result) => {
+        expect(result).toBe(true);
+      });
     });
+  });
 
-    it('should run contentStore.showContent', () => {
+  describe('showContent', () => {
+    it('should run store.dispatch(XxxContentActions.showContent)', () => {
       service.showContent(contentKey);
-      expect(mockXxxContentStore.showContent).toHaveBeenCalledWith(contentKey);
+      store.scannedActions$.subscribe((actions) => {
+        console.log(actions);
+        expect(actions).toEqual({ key: 'home', type: '[xxxContent] showContent' });
+      });
     });
   });
 });
