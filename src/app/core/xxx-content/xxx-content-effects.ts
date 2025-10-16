b72080356c5936ebd0690bfe88d0defe4cf129f7
angularexample <info@angularexample.com>
2025-10-16 15:16:53 -0400

    reorder effect and imports

--- src/app/core/xxx-content/xxx-content-effects.ts
+++ src/app/core/xxx-content/xxx-content-effects.ts
@@ -1,19 +1,30 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { catchError, filter, map, of, switchMap } from 'rxjs';
+import { concatLatestFrom } from '@ngrx/operators';
 import { HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { XxxContentActions } from './xxx-content.actions';
-import { XxxContentData } from './xxx-content-data';
-import * as XxxContentSelectors from './xxx-content.selectors';
+import { XxxContentActions } from './xxx-content-actions';
 import { XxxContentApi } from './xxx-content-types';
-import { concatLatestFrom } from '@ngrx/operators';
+import { XxxContentData } from './xxx-content-data';
+import * as XxxContentSelectors from './xxx-content-selectors';
 
 @Injectable()
 export class XxxContentEffects {
   private actions$: Actions = inject(Actions);
   private contentService: XxxContentData = inject(XxxContentData);
   private store: Store = inject(Store);
+
+  getContent$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(XxxContentActions.getContent),
+      switchMap((action: { key: string }) =>
+        this.contentService.getContent(action.key).pipe(
+          map((response: XxxContentApi) => XxxContentActions.getContentSuccess({ content: response })),
+          catchError((err: HttpErrorResponse) => of(XxxContentActions.getContentError({ key: action.key, err })))
+        )
+      )
+    ));
 
   showContent$ = createEffect(() =>
     this.actions$.pipe(
@@ -23,17 +34,6 @@ export class XxxContentEffects {
       }) => this.store.select(XxxContentSelectors.selectIsContentLoaded(action.key))),
       filter(([_action, isLoaded]) => !isLoaded),
       map(([arg1, _arg2]) => arg1),
-      map((action: { key: string }) => XxxContentActions.getContent({key: action.key}))
-    ));
-
-  getContent$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(XxxContentActions.getContent),
-      switchMap((action: { key: string }) =>
-        this.contentService.getContent(action.key).pipe(
-          map((response: XxxContentApi) => XxxContentActions.getContentSuccess({content: response})),
-          catchError((err: HttpErrorResponse) => of(XxxContentActions.getContentError({key: action.key, err})))
-        )
-      )
+      map((action: { key: string }) => XxxContentActions.getContent({ key: action.key }))
     ));
 }
