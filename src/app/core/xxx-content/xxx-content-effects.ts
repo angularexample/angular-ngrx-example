a4c508c54e692e215ce7b0a12ab4d0bb91ee621b
angularexample <info@angularexample.com>
2025-10-16 18:12:44 -0400

    remove console logs

--- src/app/core/xxx-content/xxx-content-effects.ts
+++ src/app/core/xxx-content/xxx-content-effects.ts
@@ -1,5 +1,5 @@
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, filter, map, of, switchMap, tap } from 'rxjs';
+import { catchError, filter, map, of, switchMap } from 'rxjs';
 import { concatLatestFrom } from '@ngrx/operators';
 import { inject, Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
@@ -28,15 +28,9 @@ export class XxxContentEffects {
   showContent$ = createEffect(() =>
     this.actions$.pipe(
       ofType(XxxContentActions.showContent),
-      tap((action) => {
-        console.log('showContent$ action', action);
-      }),
       concatLatestFrom((action: {
         key: string
       }) => this.store.select(XxxContentSelectors.selectIsContentLoaded(action.key))),
-      tap(([_action, isLoaded]) => {
-        console.log('showContent$ isLoaded', isLoaded);
-      }),
       filter(([_action, isLoaded]) => !isLoaded),
       map(([arg1, _arg2]) => arg1),
       map((action: { key: string }) => XxxContentActions.getContent({ key: action.key }))
