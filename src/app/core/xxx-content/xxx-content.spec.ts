e4221b5a0c99aa3499ed622578b588ba8a8a7c6f
angularexample <info@angularexample.com>
2025-10-16 14:58:07 -0400

    fix xxx-content.spec.ts, 100% coverage

--- src/app/core/xxx-content/xxx-content.spec.ts
+++ src/app/core/xxx-content/xxx-content.spec.ts
@@ -1,10 +1,11 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { mockContentHome } from './xxx-content.mocks';
 import { XxxContent } from './xxx-content';
 import { XxxContentFacade } from './xxx-content-facade';
+import { of } from 'rxjs';
 
-const mockContentKey = 'content-key';
+const mockContentKey = 'home';
 
 // To test the input, use a mock host component.
 @Component({
@@ -20,17 +21,17 @@ describe('XxxContent', () => {
   let contentComponent: XxxContent;
 
   const mockXxxContentFacade = {
-    contentByKey: jest.fn().mockReturnValue(signal(mockContentHome)),
-    isContentEmpty: jest.fn().mockReturnValue(signal(false)),
-    isContentError: jest.fn().mockReturnValue(signal(false)),
-    showContent: jest.fn(),
+    contentByKey$: jest.fn().mockReturnValue(of(mockContentHome)),
+    isContentEmpty$: jest.fn().mockReturnValue(of(false)),
+    isContentError$: jest.fn().mockReturnValue(of(false)),
+    showContent: jest.fn()
   };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HostComponent],
       providers: [
-        {provide: XxxContentFacade, useValue: mockXxxContentFacade},
+        { provide: XxxContentFacade, useValue: mockXxxContentFacade }
       ]
     }).compileComponents();
     hostFixture = TestBed.createComponent(HostComponent);
@@ -38,26 +39,20 @@ describe('XxxContent', () => {
     contentComponent = hostFixture.debugElement.children[0].componentInstance;
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   describe('constructor phase', () => {
     it('should be created', () => {
       expect(contentComponent).toBeDefined();
     });
-  })
+  });
 
   describe('OnInit', () => {
     it('should have input value', async () => {
       await hostFixture.whenStable();
       expect(contentComponent.contentKey()).toBe(mockContentKey);
-    });
-
-    it('should run contentFacade.isContentEmpty', async () => {
-      await hostFixture.whenStable();
-      expect(mockXxxContentFacade.isContentEmpty).toHaveBeenCalledWith(mockContentKey);
-    });
-
-    it('should run contentFacade.isContentError', async () => {
-      await hostFixture.whenStable();
-      expect(mockXxxContentFacade.isContentError).toHaveBeenCalledWith(mockContentKey);
     });
 
     it('should run contentFacade.showContent', async () => {
