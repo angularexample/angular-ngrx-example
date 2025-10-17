875d12e10becc95fca33215e137f92490994e41f
angularexample <info@angularexample.com>
2025-10-17 19:33:27 -0400

    add xxx-user-facade.spec.ts, 100% coverage

--- src/app/features/xxx-user/xxx-user-facade.spec.ts
+++ src/app/features/xxx-user/xxx-user-facade.spec.ts
@@ -1,65 +1,69 @@
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TestBed } from '@angular/core/testing';
+import { XxxUserActions } from './xxx-user.actions';
 import { XxxUserFacade } from './xxx-user-facade';
-import { XxxUserStore } from './xxx-user-store';
+import { xxxUserInitialState } from './xxx-user-types';
 
 describe('XxxUserFacade', () => {
-  const mockUserId=1;
-  const mockXxxUserStore = {
-    isUsersEmpty: jest.fn(),
-    isUsersLoaded: jest.fn(),
-    isUsersLoading: jest.fn(),
-    isSelectedUserId: jest.fn(),
-    selectedUserId: jest.fn(),
-    setSelectedUserId: jest.fn(),
-    showUsers: jest.fn(),
-    users: jest.fn(),
-  };
+  const mockUserId = 1;
+  let service: XxxUserFacade;
+  let spyDispatch: jest.SpyInstance;
+  let store: MockStore;
 
-  TestBed.configureTestingModule({
-    providers: [
-      XxxUserFacade,
-      {provide: XxxUserStore, useValue: mockXxxUserStore},
-    ],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ initialState: xxxUserInitialState }),
+        XxxUserFacade
+      ]
+    });
+    service = TestBed.inject(XxxUserFacade);
+    store = TestBed.inject(MockStore);
+    spyDispatch = jest.spyOn(store, 'dispatch');
   });
 
-  const service: XxxUserFacade = TestBed.inject(XxxUserFacade);
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+  });
+
   describe('constructor phase', () => {
     it('should be created', () => {
       expect(service).toBeDefined();
     });
 
-    it('should have isUsersEmpty', () => {
-      expect(service.isUsersEmpty).toBeDefined();
+    it('should have isUsersEmpty$', () => {
+      expect(service.isUsersEmpty$).toBeDefined();
     });
 
-    it('should have isUsersLoaded', () => {
-      expect(service.isUsersLoaded).toBeDefined();
+    it('should have isUsersLoaded$', () => {
+      expect(service.isUsersLoaded$).toBeDefined();
     });
 
-    it('should have isUsersLoading', () => {
-      expect(service.isUsersLoading).toBeDefined();
+    it('should have isUsersLoading$', () => {
+      expect(service.isUsersLoading$).toBeDefined();
     });
 
-    it('should have selectedUserId', () => {
-      expect(service.selectedUserId).toBeDefined();
+    it('should have selectedUserId$', () => {
+      expect(service.selectedUserId$).toBeDefined();
     });
 
-    it('should have users', () => {
-      expect(service.users).toBeDefined();
+    it('should have users$', () => {
+      expect(service.users$).toBeDefined();
     });
-  })
+  });
 
   describe('showUsers', () => {
-    it('should call userStore.showUsers', () => {
+    it('should call dispatch showUsers', () => {
       service.showUsers();
-      expect(mockXxxUserStore.showUsers).toHaveBeenCalled();
+      expect(spyDispatch).toHaveBeenCalledWith({ type: XxxUserActions.showUsers.type });
     });
   });
 
   describe('setSelectedUserId', () => {
-    it('should should call userStore.setSelectedUserId', () => {
+    it('should should call dispatch setSelectedUserId', () => {
       service.setSelectedUserId(mockUserId);
-      expect(mockXxxUserStore.setSelectedUserId).toHaveBeenCalledWith(mockUserId);
+      expect(spyDispatch).toHaveBeenCalledWith({ type: XxxUserActions.setSelectedUserId.type, userId: mockUserId });
     });
   });
 });
