18e757a38202cebdb4b50c8daa9adbd24d462489
angularexample <info@angularexample.com>
2025-10-17 19:44:24 -0400

    add xxx-post-facade.spec.ts, 100% coverage

--- src/app/features/xxx-post/xxx-post-facade.spec.ts
+++ src/app/features/xxx-post/xxx-post-facade.spec.ts
@@ -1,105 +1,104 @@
 import { mockPost } from './xxx-post.mocks';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TestBed } from '@angular/core/testing';
 import { XxxPostFacade } from './xxx-post-facade';
-import { XxxPostStore } from './xxx-post-store';
+import { xxxPostInitialState } from './xxx-post-types';
+import { XxxUserFacade } from '../xxx-user/xxx-user-facade';
+import { XxxPostActions } from './xxx-post.actions';
 
 describe('XxxPostFacade', () => {
-  const mockXxxPostStore = {
-    isNoSelectedPost: jest.fn(),
-    isNoSelectedUser: jest.fn(),
-    isPostsEmpty: jest.fn(),
-    isPostsLoaded: jest.fn(),
-    isPostsLoading: jest.fn(),
-    isSaveButtonDisabled: jest.fn(),
-    posts: jest.fn(),
-    selectedPost: jest.fn(),
-    selectedPostId: jest.fn(),
-    selectedUserId: jest.fn(),
-    setPostForm: jest.fn(),
-    setSelectedPostId: jest.fn(),
-    showPosts: jest.fn(),
-    updatePost: jest.fn(),
-  };
+  let service: XxxPostFacade;
+  let spyDispatch: jest.SpyInstance;
+  let store: MockStore;
 
-  TestBed.configureTestingModule({
-    providers: [
-      XxxPostFacade,
-      {provide: XxxPostStore, useValue: mockXxxPostStore},
-    ],
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ initialState: xxxPostInitialState }),
+        XxxUserFacade
+      ]
+    });
+    service = TestBed.inject(XxxPostFacade);
+    store = TestBed.inject(MockStore);
+    spyDispatch = jest.spyOn(store, 'dispatch');
   });
 
-  const service: XxxPostFacade = TestBed.inject(XxxPostFacade);
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+  });
+
   describe('constructor phase', () => {
     it('should be created', () => {
       expect(service).toBeDefined();
     });
 
-    it('should have isNoSelectedPost', () => {
-      expect(service.isNoSelectedPost).toBeDefined();
+    it('should have isNoSelectedPost$', () => {
+      expect(service.isNoSelectedPost$).toBeDefined();
     });
 
-    it('should have isNoSelectedUser', () => {
-      expect(service.isNoSelectedUser).toBeDefined();
+    it('should have isNoSelectedUser$', () => {
+      expect(service.isNoSelectedUser$).toBeDefined();
     });
 
-    it('should have isPostsEmpty', () => {
-      expect(service.isPostsEmpty).toBeDefined();
+    it('should have isPostsEmpty$', () => {
+      expect(service.isPostsEmpty$).toBeDefined();
     });
 
-    it('should have isPostsLoaded', () => {
-      expect(service.isPostsLoaded).toBeDefined();
+    it('should have isPostsLoaded$', () => {
+      expect(service.isPostsLoaded$).toBeDefined();
     });
 
-    it('should have isPostsLoading', () => {
-      expect(service.isPostsLoading).toBeDefined();
+    it('should have isPostsLoading$', () => {
+      expect(service.isPostsLoading$).toBeDefined();
     });
 
-    it('should have isSaveButtonDisabled', () => {
-      expect(service.isSaveButtonDisabled).toBeDefined();
+    it('should have isSaveButtonDisabled$', () => {
+      expect(service.isSaveButtonDisabled$).toBeDefined();
     });
 
-    it('should have posts', () => {
-      expect(service.posts).toBeDefined();
+    it('should have posts$', () => {
+      expect(service.posts$).toBeDefined();
     });
 
-    it('should have selectedPost', () => {
-      expect(service.selectedPost).toBeDefined();
+    it('should have selectedPost$', () => {
+      expect(service.selectedPost$).toBeDefined();
     });
 
-    it('should have selectedPostId', () => {
-      expect(service.selectedPostId).toBeDefined();
+    it('should have selectedPostId$', () => {
+      expect(service.selectedPostId$).toBeDefined();
     });
 
-    it('should have selectedUserId', () => {
-      expect(service.selectedUserId).toBeDefined();
+    it('should have selectedUserId$', () => {
+      expect(service.selectedUserId$).toBeDefined();
     });
-  })
+  });
 
   describe('setSelectedPostId', () => {
-    it('should call postStore.setSelectedPostId', () => {
+    it('should call dispatch setSelectedPostId', () => {
       service.setSelectedPostId(mockPost.id);
-      expect(mockXxxPostStore.setSelectedPostId).toHaveBeenCalledWith(mockPost.id);
+      expect(spyDispatch).toHaveBeenCalledWith({ type: XxxPostActions.setSelectedPostId.type, postId: mockPost.id });
     });
   });
 
   describe('setPostForm', () => {
-    it('should call postStore.setPostForm', () => {
+    it('should call dispatch setPostForm', () => {
       service.setPostForm(mockPost);
-      expect(mockXxxPostStore.setPostForm).toHaveBeenCalledWith(mockPost);
+      expect(spyDispatch).toHaveBeenCalledWith({ type: XxxPostActions.setPostForm.type, post: mockPost });
     });
   });
 
   describe('showPosts', () => {
-    it('should call postStores.showPosts', () => {
+    it('should call dispatch showPosts', () => {
       service.showPosts();
-      expect(mockXxxPostStore.showPosts).toHaveBeenCalled();
+      expect(spyDispatch).toHaveBeenCalledWith({ type: XxxPostActions.showPosts.type });
     });
   });
 
   describe('updatePost', () => {
-    it('should call postStores.updatePost', () => {
+    it('should call dispatch updatePost', () => {
       service.updatePost();
-      expect(mockXxxPostStore.updatePost).toHaveBeenCalled();
+      expect(spyDispatch).toHaveBeenCalledWith({ type: XxxPostActions.updatePost.type });
     });
   });
 });
