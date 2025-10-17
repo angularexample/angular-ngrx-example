cf72c096889df316b3d1e3df69de67d9ebc3605a
angularexample <info@angularexample.com>
2025-10-17 10:03:00 -0400

    fix getContentError

--- src/app/core/xxx-content/xxx-content-reducer-logic.ts
+++ src/app/core/xxx-content/xxx-content-reducer-logic.ts
@@ -1,8 +1,11 @@
 import { XxxContentApi, XxxContentState, XxxContentStatus, XxxContentType } from './xxx-content-types';
 
 export const getContent = (state: XxxContentState, action: { key: string }) => {
+  // make a new copy of the contents array
   const contents: XxxContentType[] = <XxxContentType[]>JSON.parse(JSON.stringify(state.contents));
+  // look for the content with the given key
   let content: XxxContentType | undefined = contents.find((item: XxxContentType) => item.key === action.key);
+  // if not found, add a new content with the given key and status LOADING
   if (content === undefined) {
     content = {
       key: action.key,
@@ -10,6 +13,7 @@ export const getContent = (state: XxxContentState, action: { key: string }) => {
     };
     contents.push(content);
   } else {
+    // if found, remove the model and set the status to LOADING
     content.contentModel = undefined;
     content.status = XxxContentStatus.LOADING;
   }
@@ -29,6 +33,7 @@ export const getContentError = (state: XxxContentState, action: { key: string })
     };
     contents.push(content);
   } else {
+    content.contentModel = undefined;
     content.status = XxxContentStatus.ERROR;
   }
   return {
