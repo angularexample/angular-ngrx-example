b9d8b9b4171f05aea89e3b17e110f4c2c804888c
angularexample <info@angularexample.com>
2025-10-17 18:38:42 -0400

    improve all tests

--- src/app/core/xxx-content/xxx-content-facade.spec.ts
+++ src/app/core/xxx-content/xxx-content-facade.spec.ts
@@ -1,21 +1,32 @@
-import { mockContentHome } from './xxx-content.mocks';
+import { createSelector } from '@ngrx/store';
+import { mockContentHome, mockContentState } from './xxx-content.mocks';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TestBed } from '@angular/core/testing';
+import { XxxContentActions } from './xxx-content-actions';
 import { XxxContentFacade } from './xxx-content-facade';
-import { xxxContentInitialState } from './xxx-content-types';
+import * as XxxContentSelectors from './xxx-content-selectors';
+import { XxxContentType } from './xxx-content-types';
 
 describe('XxxContentFacade', () => {
   const contentKey: string = 'home';
+  let service: XxxContentFacade;
+  let store: MockStore;
 
-  TestBed.configureTestingModule({
-    providers: [
-      provideMockStore({ initialState: xxxContentInitialState }),
-      XxxContentFacade
-    ]
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        provideMockStore({ initialState: mockContentState }),
+        XxxContentFacade
+      ]
+    });
+    service = TestBed.inject(XxxContentFacade);
+    store = TestBed.inject(MockStore);
   });
 
-  const service: XxxContentFacade = TestBed.inject(XxxContentFacade);
-  const store: MockStore = TestBed.inject(MockStore);
+  afterEach(() => {
+    jest.useRealTimers();
+    jest.clearAllMocks();
+  });
 
   describe('constructor phase', () => {
     it('should be created', () => {
@@ -36,36 +47,69 @@ describe('XxxContentFacade', () => {
   });
 
   describe('contentByKey$', () => {
-    it('should run store.select(selectContentByKey)', () => {
-      service.contentByKey$(contentKey).subscribe((result) => {
-        expect(result).toEqual(mockContentHome);
+    it('should run select(selectContentByKey)', () => {
+      // You cannot use overrideSelector for a selector with parameters,
+      // so you must use a spy with a mock return value
+      jest.spyOn(XxxContentSelectors, 'selectContentByKey').mockReturnValue(
+        createSelector(
+          (): XxxContentType[] => [],
+          (): XxxContentType | undefined => mockContentHome
+        )
+      );
+      jest.useFakeTimers();
+      let result: boolean = false;
+      service.contentByKey$(contentKey).subscribe((value: XxxContentType | undefined) => {
+        result = value?.key === mockContentHome.key &&
+          value?.contentModel?.bodyText === mockContentHome.contentModel?.bodyText &&
+          value?.contentModel?.pageTitle === mockContentHome.contentModel?.pageTitle;
       });
+      // Use runAllTimers to complete the observable subscription
+      jest.runAllTimers();
+      expect(result).toBe(true);
     });
   });
 
   describe('isContentEmpty$', () => {
-    it('should run store.select(selectIsContentEmpty)', () => {
-      service.isContentEmpty$(contentKey).subscribe((result) => {
-        expect(result).toBe(false);
+    it('should run select(selectIsContentEmpty)', () => {
+      jest.spyOn(XxxContentSelectors, 'selectIsContentEmpty').mockReturnValue(
+        createSelector(
+          (): XxxContentType | undefined => undefined,
+          (): boolean => true
+        )
+      );
+      jest.useFakeTimers();
+      let result: boolean = false;
+      service.isContentEmpty$('empty').subscribe((value) => {
+        result = value;
       });
+      jest.runAllTimers();
+      expect(result).toBe(true);
     });
   });
 
   describe('isContentError$', () => {
-    it('should run store.select(selectIsContentError)', () => {
-      service.contentByKey$(contentKey).subscribe((result) => {
-        expect(result).toBe(true);
+    it('should run select(selectIsContentError)', () => {
+      jest.spyOn(XxxContentSelectors, 'selectIsContentError').mockReturnValue(
+        createSelector(
+          (): XxxContentType | undefined => undefined,
+          (): boolean => true
+        )
+      );
+      jest.useFakeTimers();
+      let result: boolean = false;
+      service.isContentError$('error').subscribe((value) => {
+        result = value;
       });
+      jest.runAllTimers();
+      expect(result).toBe(true);
     });
   });
 
   describe('showContent', () => {
-    it('should run store.dispatch(XxxContentActions.showContent)', () => {
+    it('should run dispatch(XxxContentActions.showContent)', () => {
+      const spyDispatch = jest.spyOn(store, 'dispatch');
       service.showContent(contentKey);
-      store.scannedActions$.subscribe((actions) => {
-        console.log(actions);
-        expect(actions).toEqual({ key: 'home', type: '[xxxContent] showContent' });
-      });
+      expect(spyDispatch).toHaveBeenCalledWith({ key: contentKey, type: XxxContentActions.showContent.type });
     });
   });
 });
