6adc78c1494d5fbb5cbca7fa79d87abaed6404b4
angularexample <info@angularexample.com>
2025-10-18 11:33:12 -0400

    update xxx-content-selectors.spec.ts, 100% coverage

--- src/app/core/xxx-content/xxx-content-selectors.spec.ts
+++ src/app/core/xxx-content/xxx-content-selectors.spec.ts
@@ -1,19 +1,13 @@
 import { mockContentEmpty, mockContentError, mockContentHome, mockContentState } from './xxx-content.mocks';
-import {
-  selectContentByKey,
-  selectContents,
-  selectIsContentEmpty,
-  selectIsContentError,
-  selectIsContentLoaded
-} from './xxx-content-selectors';
+import * as XxxContentSelectors from './xxx-content-selectors';
 import { XxxContentType } from './xxx-content-types';
 
-describe('xxx-content=selectors', () => {
+describe('xxx-content-selectors', () => {
 
   describe('selectContents', () => {
     it('should return expected contents', () => {
       const expectedResult: XxxContentType[] = mockContentState.contents;
-      const result: unknown = selectContents.projector(mockContentState);
+      const result: unknown = XxxContentSelectors.selectContents.projector(mockContentState);
       expect(result).toEqual(expectedResult);
     });
   });
@@ -21,58 +15,58 @@ describe('xxx-content=selectors', () => {
   describe('selectContentByKey', () => {
     it('should return expected content', () => {
       const expectedResult: XxxContentType = mockContentHome;
-      const result: unknown = selectContentByKey(mockContentHome.key).projector(mockContentState.contents);
+      const result: unknown = XxxContentSelectors.selectContentByKey(mockContentHome.key).projector(mockContentState.contents);
       expect(result).toEqual(expectedResult);
     });
   });
 
   describe('selectIsContentLoaded', () => {
     it('should return true when loaded', () => {
-      const result: unknown = selectIsContentLoaded(mockContentHome.key).projector(mockContentHome);
+      const result: unknown = XxxContentSelectors.selectIsContentLoaded(mockContentHome.key).projector(mockContentHome);
       expect(result).toBe(true);
     });
 
     it('should return false when content not found', () => {
-      const result: unknown = selectIsContentLoaded('none').projector(undefined);
+      const result: unknown = XxxContentSelectors.selectIsContentLoaded('none').projector(undefined);
       expect(result).toBe(false);
     });
 
     it('should return false when content status is not loaded', () => {
-      const result: unknown = selectIsContentLoaded('empty').projector(mockContentEmpty);
+      const result: unknown = XxxContentSelectors.selectIsContentLoaded('empty').projector(mockContentEmpty);
       expect(result).toBe(false);
     });
   });
 
   describe('selectIsContentEmpty', () => {
     it('should return true when status is empty', () => {
-      const result: unknown = selectIsContentEmpty(mockContentHome.key).projector(mockContentEmpty);
+      const result: unknown = XxxContentSelectors.selectIsContentEmpty(mockContentHome.key).projector(mockContentEmpty);
       expect(result).toBe(true);
     });
 
     it('should return false when status is loaded', () => {
-      const result: unknown = selectIsContentEmpty(mockContentHome.key).projector(mockContentHome);
+      const result: unknown = XxxContentSelectors.selectIsContentEmpty(mockContentHome.key).projector(mockContentHome);
       expect(result).toBe(false);
     });
 
     it('should return false when content not found', () => {
-      const result: unknown = selectIsContentEmpty('none').projector(undefined);
+      const result: unknown = XxxContentSelectors.selectIsContentEmpty('none').projector(undefined);
       expect(result).toBe(false);
     });
   });
 
   describe('selectIsContentError', () => {
     it('should return true when status is error', () => {
-      const result: unknown = selectIsContentError('error').projector(mockContentError);
+      const result: unknown = XxxContentSelectors.selectIsContentError('error').projector(mockContentError);
       expect(result).toBe(true);
     });
 
     it('should return false when status is loaded', () => {
-      const result: unknown = selectIsContentError(mockContentHome.key).projector(mockContentHome);
+      const result: unknown = XxxContentSelectors.selectIsContentError(mockContentHome.key).projector(mockContentHome);
       expect(result).toBe(false);
     });
 
     it('should return false when content not found', () => {
-      const result: unknown = selectIsContentError('none').projector(undefined);
+      const result: unknown = XxxContentSelectors.selectIsContentError('none').projector(undefined);
       expect(result).toBe(false);
     });
   });
