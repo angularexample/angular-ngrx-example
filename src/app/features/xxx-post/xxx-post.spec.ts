b2d6da53b08f46adfac2d149bb4687a3c31d5ccc
angularexample <info@angularexample.com>
2025-10-18 09:18:54 -0400

    add xxx-post.spec.ts, 100% coverage

--- src/app/features/xxx-post/xxx-post.spec.ts
+++ src/app/features/xxx-post/xxx-post.spec.ts
@@ -1,6 +1,8 @@
+import { AsyncPipe } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { mockPost, mockPosts } from './xxx-post.mocks';
+import { of } from 'rxjs';
 import { XxxContent } from '../../core/xxx-content/xxx-content';
 import { XxxContentFacade } from '../../core/xxx-content/xxx-content-facade';
 import { XxxPost } from './xxx-post';
@@ -11,10 +13,11 @@ import { XxxPostType } from './xxx-post-types';
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    XxxContent,
+    AsyncPipe,
+    XxxContent
   ],
   selector: 'extended-xxx-post',
-  templateUrl: './xxx-post.html',
+  templateUrl: './xxx-post.html'
 })
 class ExtendedXxxPost extends XxxPost {
   override selectPost(post: XxxPostType) {
@@ -27,32 +30,34 @@ describe('XxxPost', () => {
   let fixture: ComponentFixture<ExtendedXxxPost>;
 
   const mockXxxContentFacade = {
-    contentByKey: jest.fn(),
-  }
+    contentByKey$: jest.fn(),
+    isContentEmpty$: jest.fn().mockReturnValue(of(false)),
+    isContentError$: jest.fn().mockReturnValue(of(false))
+  };
 
   const mockXxxPostFacade = {
-    isNoSelectedPost: jest.fn().mockReturnValue(signal(false)),
-    isNoSelectedUser: jest.fn().mockReturnValue(signal(false)),
-    isPostsEmpty: jest.fn().mockReturnValue(signal(false)),
-    isPostsLoaded: jest.fn().mockReturnValue(signal(false)),
-    isPostsLoading: jest.fn().mockReturnValue(signal(false)),
-    isSaveButtonDisabled: jest.fn().mockReturnValue(signal(false)),
-    posts: jest.fn().mockReturnValue(signal(mockPosts)),
-    selectedPost: jest.fn().mockReturnValue(signal(mockPost)),
-    selectedPostId: jest.fn().mockReturnValue(signal(mockPost.id)),
-    selectedUserId: jest.fn().mockReturnValue(signal(mockPost.userId)),
+    isNoSelectedPost$: jest.fn().mockReturnValue(of(false)),
+    isNoSelectedUser$: jest.fn().mockReturnValue(of(false)),
+    isPostsEmpty$: jest.fn().mockReturnValue(of(false)),
+    isPostsLoaded$: jest.fn().mockReturnValue(of(false)),
+    isPostsLoading$: jest.fn().mockReturnValue(of(false)),
+    isSaveButtonDisabled$: jest.fn().mockReturnValue(of(false)),
+    posts$: jest.fn().mockReturnValue(of(mockPosts)),
+    selectedPost$: jest.fn().mockReturnValue(of(mockPost)),
+    selectedPostId$: jest.fn().mockReturnValue(of(mockPost.id)),
+    selectedUserId$: jest.fn().mockReturnValue(of(mockPost.userId)),
     setPostForm: jest.fn(),
     setSelectedPostId: jest.fn(),
     showPosts: jest.fn(),
-    updatePost: jest.fn(),
-  }
+    updatePost: jest.fn()
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ExtendedXxxPost],
       providers: [
-        {provide: XxxContentFacade, useValue: mockXxxContentFacade},
-        {provide: XxxPostFacade, useValue: mockXxxPostFacade}
+        { provide: XxxContentFacade, useValue: mockXxxContentFacade },
+        { provide: XxxPostFacade, useValue: mockXxxPostFacade }
       ]
     }).compileComponents();
     fixture = TestBed.createComponent(ExtendedXxxPost);
