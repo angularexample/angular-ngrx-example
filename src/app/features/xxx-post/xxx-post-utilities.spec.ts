b34d8e35a61c8e13fa429e1527f53c7b588a1e81
angularexample <info@angularexample.com>
2025-10-18 18:37:13 -0400

    fix xxx-post-utilities.spec.ts, 100% coverage

--- src/app/features/xxx-post/xxx-post-utilities.spec.ts
+++ src/app/features/xxx-post/xxx-post-utilities.spec.ts
@@ -1,18 +1,26 @@
 import { isPostsEqual } from './xxx-post-utilities';
-import { mockPost, mockPost1, mockPost2 } from './xxx-post.mocks';
+import { mockPost1, mockPost2 } from './xxx-post.mocks';
 
 describe('xxx-post-utilities', () => {
   describe('isPostsEqual', () => {
     it('should return true when post objects are equal', () => {
-      expect(isPostsEqual(mockPost, mockPost1)).toBe(true);
+      expect(isPostsEqual(mockPost1, mockPost1)).toBe(true);
     });
 
     it('should return false when post objects are not equal', () => {
       expect(isPostsEqual(mockPost1, mockPost2)).toBe(false);
     });
 
-    it('should return false when one post object is undefined', () => {
+    it('should return false when first post object is undefined', () => {
+      expect(isPostsEqual(undefined,  mockPost1)).toBe(false);
+    });
+
+    it('should return false when second post object is undefined', () => {
       expect(isPostsEqual(mockPost1, undefined)).toBe(false);
+    });
+
+    it('should return false when both post objects are undefined', () => {
+      expect(isPostsEqual(undefined, undefined)).toBe(false);
     });
   });
 });
